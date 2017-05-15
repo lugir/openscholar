6e5be43d885db7995d4c0b544a2a96cf8a26794d
Somenath Dey <somnath2prl@gmail.com>
2017-05-15 17:07:03 +0530

    9559: Add exempted content type in array.

---
+++ openscholar/modules/os/modules/os_restful/plugins/restful/node/contents/1.0/ContentsRestfulBase.class.php
@@ -0,0 +1,138 @@
+<?php
+
+class ContentsRestfulBase extends RestfulEntityBase {
+
+
+  /**
+   * Define the bundles to exposed to the API.
+   *
+   * @var array
+   *  Array keyed by bundle machine, and the RESTful resource as the value.
+   */
+
+  protected $bundles = array(
+    'personal' => 'personal',
+    );
+
+  /**
+   * Return the bundles.
+   *
+   * @return array
+   *  An array of the exposed bundles as key and resource as value.
+   */
+  protected function getBundles() {
+    return $this->bundles;
+  }
+
+   /**
+   * Overrides RestfulEntityBase::getQueryForList().
+   */
+  public function getQueryForList() {
+    $query = parent::getQueryForList();
+    $query->entityCondition('bundle', array_keys($this->getBundles()), 'NOT IN');
+    return $query;
+  }
+
+
+
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['type'] = array(
+      'property' => 'type',
+    );
+
+    $public_fields['publish_status'] = array(
+      'property' => 'status',
+    );
+
+    $public_fields['author'] = array(
+      'property' => 'author',
+      'sub_property' => 'name',
+    );
+
+    $public_fields['changed'] = array(
+      'property' => 'changed',
+      'process_callbacks' => array(
+        array($this, 'dateFormat'),
+      ),
+    );
+
+    $public_fields['alias'] = array(
+      'callback' => array($this, 'getEntityAlias'),
+    );
+
+    return $public_fields;
+  }
+
+  /**
+   * Get entity's alias.
+   *
+   * @param \EntityDrupalWrapper $wrapper
+   *   The wrapped entity.
+   *
+   * @return string
+   *   The alias URL.
+   */
+  protected function getEntityAlias(\EntityDrupalWrapper $wrapper) {
+
+    $values = $wrapper->value();
+    //print_r($values);
+    return $values->path['alias'];
+  }
+
+  protected function dateFormat($timestamp) {
+    return format_date($timestamp, $type = 'long');
+  }
+
+ /* protected function checkEntityAccess($op, $entity_type, $entity) {
+    $request = $this->getRequest();
+
+    if ($request['vsite']) {
+      spaces_set_space(spaces_load('og', $request['vsite']));
+    }
+
+    if (empty($entity->nid)) {
+      // This is still a new node. Skip.
+      return;
+    }
+
+    if ($is_group = og_is_group($entity_type, $entity)) {
+      $group = $entity;
+    }
+    else {
+      $wrapper = entity_metadata_wrapper('node', $entity);
+      $group = $wrapper->{OG_AUDIENCE_FIELD}->get(0)->value();
+    }
+
+    if (empty($request['vsite'])) {
+      spaces_set_space(spaces_load('og', $group->nid));
+    }
+
+    $manager = og_user_access('node', $group->nid, 'administer users', $this->getAccount());
+
+    if ($is_group) {
+      // In addition to the node access check, we need to see if the user can
+      // manage groups.
+      return $manager && !vsite_access_node_access($group, 'view', $this->getAccount()) == NODE_ACCESS_DENY;
+    }
+    else {
+      $app = os_get_app_by_bundle($entity->type);
+      $space = spaces_get_space();
+      $application_settings = $space->controllers->variable->get('spaces_features');
+
+      switch ($application_settings[$app]) {
+        case OS_DISABLED_APP:
+          return FALSE;
+
+        case OS_PRIVATE_APP:
+          return og_is_member('node', $group->nid, 'user', $this->getAccount()) && parent::checkEntityAccess($op, $entity_type, $entity);
+
+        default:
+        case OS_PUBLIC_APP:
+          return parent::checkEntityAccess($op, $entity_type, $entity);
+      }
+    }
+  }*/
+
+}
